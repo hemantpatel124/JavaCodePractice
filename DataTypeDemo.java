9523f41da5f541bfa3887852b327c8d75fd97777
hemant07 <hemantpatel124@gmail.com>
2023-05-02 23:47:09 +0530

    DataTypeDemo file update

--- DataTypeDemo.java
+++ DataTypeDemo.java
@@ -1,38 +1,32 @@
 public class DataTypeDemo {
 	public static void main(String[] args) {
 		//1.byte 2.short 3.int 4.long 5.float 6.double 7.char 8.boolean
-		double f = 4.234634573457;
-		System.out.println(f);
-		int i=10;
-		int j=20;
-		Integer k = new Integer(12);
-		System.out.println(k);
-		System.out.println(i+j);
+		
+		
+		int i = 7;
+		char c = 'H';
+		float f = 1.0f;
+		double d = 1.0;
+		long l = 27;
+		boolean b = true;
+		String s = "Hemant";
+		
+		// All type datatype variable print
+		System.out.println("\nInteger variable: " + i + "\n Character variable: " + c + "\n Float variable: " + f
+		        + "\n double variable: " + d + "\n long variable: " + l + "\n boolean variable: " + b
+		        + "\n String variable: " + s);
+
+		// sum operation performed
+		System.out.println("\nSum: " + (l + i));
+
+		// subtraction operation performed
+		System.out.println("Subtraction: " + (l - i));
+
+		// Multiplication operation performed
+		System.out.println("Multiplication: " + (l * i));
+		
+		
 	}
 }
 
 
-/*
-//variable declaration
-int i = 7;
-char c = 'H';
-float f = 1.0f;
-double d = 1.0;
-long l = 27;
-boolean b = true;
-String s = "Hemant";
-
-// All type datatype variable print
-System.out.println("\nInteger variable: " + i + "\n Character variable: " + c + "\n Float variable: " + f
-        + "\n double variable: " + d + "\n long variable: " + l + "\n boolean variable: " + b
-        + "\n String variable: " + s);
-
-// sum operation performed
-System.out.println("\nSum: " + (l + i));
-
-// subtraction operation performed
-System.out.println("Subtraction: " + (l - i));
-
-// Multiplication operation performed
-System.out.println("Multiplication: " + (l * i));
-*/
